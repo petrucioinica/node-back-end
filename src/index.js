dfb147562cdac02abec2c55e6ea49831b6876652
JohnnySavu <johnny.savu.99@gmail.com>
2021-12-03 23:11:05 +0200

    second_commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const port = process.env.PORT;
+console.log("port is ", port);
+
+//sequelize
+const db = require("../models/index.js");
+db.sequelize.sync({ alter: true });
+
+const app = express();
+
+app.listen(port, () => {
+	console.log("App started, hello world!");
+});
+
+app.get("/", (req, res) => {
+	res.send("Hello!");
+});
